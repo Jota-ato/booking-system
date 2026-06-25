4b6654d7709c0cf42fcfe40378c7ef6896dce4ff
Julio César Zavala Blanco <julioc-01@outlook.com>
2026-06-25 10:23:46 -0600

    refactor(add-extra-card.tsx): tiny change, improve the design

--- src/features/services/components/add-extra-card.tsx
+++ src/features/services/components/add-extra-card.tsx
@@ -10,10 +10,10 @@ export function AddExtraCard({
             trigger={
                 <Button
                     variant="outline"
-                    className="group flex h-full min-h-62.5 w-full flex-col items-center justify-center gap-4 rounded-xl border-2 border-dashed border-muted-foreground/25 bg-transparent transition-all duration-300 hover:border-primary/50 hover:bg-muted/30"
+                    className="group flex h-full  w-full flex-col items-center justify-center gap-2 rounded-xl border-2 border-dashed border-muted-foreground/25 bg-transparent transition-all duration-300 hover:border-primary/50 hover:bg-muted/30"
                 >
-                    <div className="rounded-full bg-primary/10 p-4 text-primary transition-transform duration-300 group-hover:scale-110 group-hover:bg-primary/20">
-                        <Plus className="size-8" />
+                    <div className="rounded-full bg-primary/10 p-2 text-primary transition-transform duration-300 group-hover:scale-110 group-hover:bg-primary/20">
+                        <Plus className="size-6" />
                     </div>
 
                     <div className="space-y-1 text-center whitespace-normal">
