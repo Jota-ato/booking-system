045fbacb85557ec736308f413663c19e938b15be
Julio César Zavala Blanco <julioc-01@outlook.com>
2026-06-25 19:29:38 -0600

    fix(services): fix problem with dimensions, now on mobile its vertical -split into two components service-page-tabs-list.tsx and service-page-tabs.tsx -use useIsMobile hook to determine wheter is mobile or not -adjust grid cols to sm: and md: instead of md: and lg:

--- src/app/admin/services/page.tsx
+++ src/app/admin/services/page.tsx
@@ -1,21 +1,13 @@
-import { AddExtraCard } from "@/features/services/components/add-extra-card";
-import { AddServiceCard } from "@/features/services/components/add-service-card";
-import { ExtraCard } from "@/features/services/components/extra-card";
+
 import { ExtraDialog } from "@/features/services/components/extra-dialog";
-import { ServiceCard } from "@/features/services/components/service-card";
 import { ServiceDialog } from "@/features/services/components/service-dialog";
+import { ServicePageTabs } from "@/features/services/components/service-page-tabs";
 import { extrasService } from "@/features/services/services/extras-service";
 import { servicesService } from "@/features/services/services/services-service";
 import { Heading } from "@/shared/components/typography/heading";
-
 import { Container } from "@/shared/components/ui/container";
 import { Separator } from "@/shared/components/ui/separator";
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger
-} from "@/shared/components/ui/tabs";
+
 
 export default async function ServicesPage() {
 
@@ -31,73 +23,13 @@ export default async function ServicesPage() {
       <Container>
         <Heading>Services</Heading>
         <Separator className="my-8" />
-        <Tabs defaultValue="activeServices">
-          <TabsList>
-            <TabsTrigger value="activeServices">Active services</TabsTrigger>
-            <TabsTrigger value="unactiveServices">Unactive services</TabsTrigger>
-            <TabsTrigger value="activeExtras">Active extras</TabsTrigger>
-            <TabsTrigger value="unactiveExtras">Unactive extras</TabsTrigger>
-          </TabsList>
-          <TabsContent className="min-h-120" value="activeServices">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-              {activeServices.length ?
-                activeServices
-                  .map(service => (
-                    <ServiceCard
-                      key={service.data.id}
-                      service={service}
-                      isAdmin
-                    />
-                  ))
-                : (
-                  <Heading level={2} className="md:col-span-2 lg:col-span-3 my-4">No active services</Heading>
-                )
-              }
-              <AddServiceCard
-                extras={extras}
-              />
-
-            </div>
-          </TabsContent>
-          <TabsContent className="min-h-120" value="unactiveServices">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-              {unactiveServices.length ?
-                unactiveServices
-                  .map(service => (
-                    <ServiceCard
-                      key={service.data.id}
-                      service={service}
-                      isAdmin
-                    />
-                  ))
-                : (
-                  <Heading level={2} className="md:col-span-2 lg:col-span-3 my-4">No unactive services</Heading>
-                )
-              }
-            </div>
-          </TabsContent>
-          <TabsContent className="min-h-120" value="activeExtras">
-            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
-              <AddExtraCard />
-              {activeExtras.length ?
-                activeExtras.map(extra => (
-                  <ExtraCard key={extra.id} extra={extra} />
-                ))
-                : <Heading level={2} className="md:col-span-3 lg:col-span-4 my-4">No extras</Heading>
-              }
-            </div>
-          </TabsContent>
-          <TabsContent className="min-h-120" value="unactiveExtras">
-            <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
-              {unactiveExtras.length ?
-                unactiveExtras.map(extra => (
-                  <ExtraCard key={extra.id} extra={extra} />
-                ))
-                : <Heading level={2} className="md:col-span-3 lg:col-span-4 my-4">No unactive extras</Heading>
-              }
-            </div>
-          </TabsContent>
-        </Tabs>
+        <ServicePageTabs
+          activeExtras={activeExtras}
+          activeServices={activeServices}
+          extras={extras}
+          unactiveExtras={unactiveExtras}
+          unactiveServices={unactiveServices}
+        />
       </Container>
       <ServiceDialog
         extras={activeExtras}
