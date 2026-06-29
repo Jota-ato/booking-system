fe7906b9886b9833fe71b56d8c1b30f0b2b1d600
Julio César Zavala Blanco <julioc-01@outlook.com>
2026-06-29 13:07:38 -0600

    feat(components): add progress bar and title to page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,10 +1,19 @@
+import { Heading } from "@/shared/components/typography/heading";
 import { ThemeToggle } from "@/shared/components/ui/toggle-theme";
+import { Metadata } from "next";
 import Link from "next/link";
+
+const title = "Booking System"
+
+export const metadata: Metadata = {
+    title,
+    description: "Booking system for beauty salon, clinics and other service-based businesses.",
+}
 
 export default function Home() {
     return (
         <>
-            <h1>Hola mundo! :3</h1>
+            <Heading>{title}</Heading>
             <nav>
                 <Link href="/agenda" className="text-blue-500 hover:underline">
                     Go to agenda
