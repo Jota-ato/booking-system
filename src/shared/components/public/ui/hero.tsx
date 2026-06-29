2930a2836c3ad6bc5f9d1ccfbfc2a3e9b91875c5
Julio César Zavala Blanco <julioc-01@outlook.com>
2026-06-29 17:36:14 -0600

    feat(hero.tsx): improve design removing container

--- src/shared/components/public/ui/hero.tsx
+++ src/shared/components/public/ui/hero.tsx
@@ -4,11 +4,9 @@ import { HeroRightColumn } from "./hero-right-column";
 export function Hero() {
     return (
         <section className="relative py-8 w-full overflow-hidden bg-background flex items-center justify-center">
-            <div className="max-w-6xl mx-auto px-6 lg:px-8">
-                <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
-                    <HeroLeftColumn />
-                    <HeroRightColumn />
-                </div>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+                <HeroLeftColumn />
+                <HeroRightColumn />
             </div>
         </section>
     );
